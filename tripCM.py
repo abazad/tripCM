020aa2aaff3f9f36c73455cee9d23a22649b13a0
Gabriele Salvatori <salvatorigabriele@gmail.com>
2012-11-20 18:26:46 +0100

    added "URL" and "mode" parameters from sys.argv

--- tripCM.py
+++ tripCM.py
@@ -1,34 +1,70 @@
-import urllib2,re,hashlib
+import os, re, sys, getopt
+import urllib2
+
+# HTML parsing library
 from bs4 import BeautifulSoup
 
 
-## PARSING HTML FILE ##
-request = urllib2.Request("http://download.cyanogenmod.com/?device=p970")
-response = urllib2.urlopen(request)
-soup = BeautifulSoup(response)
-output = soup.find('a', href=re.compile('^http://get.cm/get'))['href']
-##     -----         ##
+def main ():
 
-## DOWNLOADING FILE ##
+    if (len (sys.argv) < 2):
 
-file_name = output.split('/')[-1]
-u = urllib2.urlopen(output)
-f = open(file_name, 'wb')
-meta = u.info()
-file_size = int(meta.getheaders("Content-Length")[0])
-print "Downloading: %s Bytes: %s" % (file_name, file_size)
+		print "Usage : python tripCM.py URL mode \n"
+		"URL : Cyanogenmod ROM URL \n"
+		"mode : stable/RC/snapshot/nightly"
 
-file_size_dl = 0
-block_sz = 8192
-while True:
-    buffer = u.read(block_sz)
-    if not buffer:
-        break
+	else:
 
-    file_size_dl += len(buffer)
-    f.write(buffer)
-    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-    status = status + chr(8)*(len(status)+1)
-    print status,
+		URL = sys.argv[1]
+	  	mode = sys.argv[2]
 
-f.close()
+	  	if mode == 'stable':
+	  		link = URL+'&type=stable'
+	  		download(link)
+
+	  	elif mode == 'RC':
+	  		link = URL+'&type=RC'
+	  		download(link)
+
+	  	elif mode == 'snapshot':
+	  	    link = URL+'&type=snapshot'
+	  	    download(link)
+
+	  	elif mode == 'nightly':
+	  		link = URL+'&type=nightly'
+	  		download(link)
+
+
+
+def download (URL):
+
+	request = urllib2.Request(URL)
+	response = urllib2.urlopen(request)
+	soup = BeautifulSoup(response)
+	output = soup.find('a', href=re.compile('^http://get.cm/get'))['href']
+
+	file_name = output.split('/')[-1]
+	u = urllib2.urlopen(output)
+	f = open(file_name, 'wb')
+	meta = u.info()
+	file_size = int(meta.getheaders("Content-Length")[0])
+	print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+	file_size_dl = 0
+	block_sz = 8192
+
+	while True:
+
+		buffer = u.read(block_sz)
+		if not buffer:
+			break
+
+			file_size_dl += len(buffer)
+			f.write(buffer)
+			status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+			status = status + chr(8)*(len(status)+1)
+			print status,
+			f.close()
+
+			if __name__ == '__main__':
+				main()
