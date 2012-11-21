211a775bd8509478094a318584a1f55c5c87ef11
Gabriele Salvatori <salvatorigabriele@gmail.com>
2012-11-21 01:18:26 +0100

    No parser needed, using regex.

--- tripCM.py
+++ tripCM.py
@@ -1,59 +1,50 @@
 import os, re, sys
 import urllib2
 
-# HTML parsing library
-from bs4 import BeautifulSoup
-
-
-def download (URL):
-
-	request = urllib2.Request(URL)
-	response = urllib2.urlopen(request)
-	soup = BeautifulSoup(response)
-	output = soup.find('a', href=re.compile('^http://get.cm/get'))['href']
-
-	file_name = output.split('/')[-1]
-	u = urllib2.urlopen(output)
-	f = open(file_name, 'wb')
-	meta = u.info()
-	file_size = int(meta.getheaders("Content-Length")[0])
-	print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-	file_size_dl = 0
-	block_sz = 8192
-
-	while True:
-
-		buffer = u.read(block_sz)
-		if not buffer:
-			break
-
-			file_size_dl += len(buffer)
-			f.write(buffer)
-			status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-			status = status + chr(8)*(len(status)+1)
-			print status,
-			f.close()
-
-
-
+#thanks to slacknux > https://github.com/slacknux for some suggestmens
+ 
 def main ():
 
-	if (len (sys.argv) < 2):
+    if (len (sys.argv) < 2):
+ 
+                print "Usage : python tripCM.py URL mode \n"
+                "URL : Cyanogenmod ROM URL \n"
+                "mode : stable/RC/snapshot/nightly"
 
-		print "Usage : python tripCM.py URL mode \n"
-		"URL : Cyanogenmod ROM URL \n"
-		"mode : stable/RC/snapshot/nightly"
+                sys.exit()
+ 
+                URL = sys.argv[1]
+                mode = sys.argv[2]
+                link = "%s&type=%s" % (URL, mode)
+                download(link)
+ 
+ 
+def download (URL):
 
-	else:
+    response = urllib2.urlopen(URL)
+    output = re.search('url=(.*?\.zip)', response.read()).group(1)
 
-		URL = sys.argv[1]
-	  	mode = sys.argv[2]
+    file_name = output.split('/')[-1]
+    u = urllib2.urlopen(output)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
 
-                # thanks to slacknux > https://github.com/slacknux for the suggestion.
-	  	link = "%s&type=%s" % (URL, mode)
-	  	download(link)
+    file_size_dl = 0
+    block_sz = 8192
 
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
 
-	  	if __name__ == '__main__':
-				main()
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            status = status + chr(8)*(len(status)+1)
+            print status,
+            f.close()
+
+            if __name__ == '__main__':
+                main()
